1391f0203d05a2b5686d8480869f3c0aea29d4fa
Ishrat Jahan Mim <ishratjahan7711@gmail.com>
2025-06-26 21:08:32 +0600

    Commit changes

---
+++ lib/login_page.dart
@@ -0,0 +1,135 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:project/main2.dart';
+import 'ChatPage.dart';
+import 'signup_page.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage>
+    with SingleTickerProviderStateMixin {
+  final _formKey = GlobalKey<FormState>();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  late AnimationController _controller;
+  late Animation<Color?> _bgColor;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+        vsync: this, duration: const Duration(seconds: 4))
+      ..repeat(reverse: true);
+    _bgColor = ColorTween(
+      begin: Colors.white,
+      end: const Color(0xFFF2F2F2),
+    ).animate(_controller);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  void _login() async{
+    if (_formKey.currentState!.validate()) {
+      try {
+        await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: emailController.text.trim(),
+          password: passwordController.text.trim(),
+        );
+
+        /*if (emailController.text == "test@gmail.com" &&
+          passwordController.text == "123456") {*/
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (_) =>
+              ChatApp(
+                //recipeId: 'recipe123',
+                //userId: 'dummyUser123',
+
+              )),
+        );
+      }on FirebaseAuthException catch (e) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text(e.message ?? "Invalid email or password")),
+        );
+      }
+      /*} else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text("Invalid email or password")),
+        );
+      }*/
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (context, child) => Scaffold(
+        backgroundColor: _bgColor.value,
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(24),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text("Login",
+                      style:
+                      TextStyle(fontSize: 28, fontWeight: FontWeight.bold)),
+                  const SizedBox(height: 20),
+                  TextFormField(
+                    controller: emailController,
+                    decoration: const InputDecoration(
+                        labelText: "Email", border: OutlineInputBorder()),
+                    validator: (value) => value!.contains('@')
+                        ? null
+                        : "Enter a valid email",
+                  ),
+                  const SizedBox(height: 16),
+                  TextFormField(
+                    controller: passwordController,
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                        labelText: "Password", border: OutlineInputBorder()),
+                    validator: (value) => value!.length >= 6
+                        ? null
+                        : "Minimum 6 characters",
+                  ),
+                  const SizedBox(height: 24),
+                  ElevatedButton(
+                    onPressed: _login,
+                    child: const Text("Login"),
+                  ),
+                  const SizedBox(height: 12),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (_) => const SignUpPage()),
+                      );
+                    },
+                    child: const Text("Don't have an account? Sign Up"),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
